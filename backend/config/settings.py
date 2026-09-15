78ae58b316a69c8f296ef52804285de4e877d349
dineshg009 <dineshg009@gmail.com>
2026-09-15 15:12:16 +0530

    new changes

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -89,6 +89,7 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_RENDERER_CLASSES": [
         "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
     ],
 }
 
